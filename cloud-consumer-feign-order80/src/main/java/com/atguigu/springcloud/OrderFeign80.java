f2438e12d5cf90977a21c146df2f320730ee87a6
曹靖 <caojing_123456@sina.com>
2023-02-14 00:00:25 +0800

    服务降级-生产者端fallback

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeign80.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeign80.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud;
 import cn.hutool.db.sql.Order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class OrderFeign80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderFeign80.class, args);
--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -1,5 +1,8 @@
 package com.atguigu.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCollapser;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.lang.management.ThreadMXBean;
@@ -16,14 +19,23 @@ public class PaymentService {
         return "线程池：" + Thread.currentThread().getName() + "  paymentInfo_ok,id: " + id + "~~~";
     }
 
+    // 3s以内走正常业务逻辑，超过三秒则调paymentInfoTimeOutHandler方法
+    @HystrixCommand(fallbackMethod = "paymentInfoTimeOutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
+    })
     public String paymentInfoTimeOut(Integer id) {
-        int timeNumber = 3;
+        int timeNumber = 2;
         try {
             TimeUnit.SECONDS.sleep(timeNumber);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return "线程池：" + Thread.currentThread().getName() + "paymentInfo_timeOut;id" + id + "耗时：" + timeNumber + "s";
+    }
+
+    // 服务降级调用的方法
+    public String paymentInfoTimeOutHandler(Integer id) {
+        return "线程池：" + Thread.currentThread().getName() + "paymentInfoTimeOutHandler;id" + id + "~~~~";
     }
 
 }
